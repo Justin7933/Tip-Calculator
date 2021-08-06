0903b7f4e14fad60911d2f4b70a3ca26ee5b1d09
Justin Damon <justindamon84@gmail.com>
2021-08-06 14:51:47 -0400

    finished

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelector('#tip-form').onchange = function(){
+
+    let bill = Number(document.getElementById('billTotal').value);
+    let tip = document.getElementById('tipInput').value;
+    document.getElementById('tipOutput').innerHTML = `${tip}%`;
+    let tipValue = bill * (tip/100)
+    let finalBill = bill + tipValue
+  console.log(finalBill)
+  let tipAmount = document.querySelector('#tipAmount')
+  let totalBillWithTip = document.querySelector('#totalBillWithTip')
+  
+  tipAmount.value = tipValue.toFixed(2);
+   totalBillWithTip.value =finalBill.toFixed(2);
+  
+   //Results
+  
+    document.getElementById('results').style.display='block'
+  }
+  
+
+
+
+
+
+
+
+
